6a2fc28d33daaf14bd7fef7e3989f293e0f188e5
Erick Daniel <erick.olano@gmail.com>
2024-10-19 00:56:25 -0500

    seccion 11 - cap. 152 - Instalar PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface City {
+  // Definimos 2 propiedades para la interfaz City: name y code
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'pipesApp';
+export class AppComponent implements OnInit {
+  // 1. Definimos una propiedad cities que es un array de objetos City o undefined
+  cities: City[] | undefined;
+  // 2. Definimos una propiedad selectedCity que es un objeto City o undefined
+  selectedCity: City | undefined;
+  // 3. Definimos el método ngOnInit que se ejecuta cuando se inicializa el componente
+  ngOnInit() {
+      // 4. Asignamos un array de objetos City a la propiedad cities
+      this.cities = [
+          { name: 'New York', code: 'NY' },
+          { name: 'Rome', code: 'RM' },
+          { name: 'London', code: 'LDN' },
+          { name: 'Istanbul', code: 'IST' },
+          { name: 'Paris', code: 'PRS' }
+      ];
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button'; // 1. Importamos ButtonModule de PrimeNG
+import { DropdownModule } from 'primeng/dropdown'; // 2. Importamos DropdownModule de PrimeNG
+import { FormsModule } from '@angular/forms'; // 3. Importamos FormsModule para usar ngModel
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // 4. Importamos BrowserAnimationsModule para habilitar animaciones
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule, // 5. Agregamos ButtonModule a la lista de imports
+    DropdownModule, // 6. Agregamos DropdownModule a la lista de imports
+    FormsModule, // 7. Agregamos FormsModule a la lista de imports
+    BrowserAnimationsModule // 8. Agregamos BrowserAnimationsModule a la lista de imports
   ],
   providers: [],
   bootstrap: [AppComponent]
