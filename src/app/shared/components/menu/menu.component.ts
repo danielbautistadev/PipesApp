43dfaf97d7e282912c150a582c0fbaae666785c6
Erick Daniel <erick.olano@gmail.com>
2024-10-20 00:34:43 -0500

    Cap. 156 - Navegar usando el MenuBar -

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -10,6 +11,8 @@ export class MenuComponent {
   
   public items: MenuItem[] | undefined; // Declaramos una propiedad 'items' de tipo 'MenuItem[]' o 'undefined' para almacenar los elementos del menú.
 
+  constructor(private router: Router) {} // Inyectamos el servicio 'Router' en el constructor del componente para poder navegar entre rutas.
+
   ngOnInit() {
       this.items = [
           {
@@ -18,15 +21,27 @@ export class MenuComponent {
             items: [
               {
                 label: 'Textos y Fechas',
-                icon: 'pi pi-align-left'
+                icon: 'pi pi-align-left',
+                // route: '/' // Establecemos la ruta a la que se navegará al hacer clic en este elemento del menú.
+                // command: () => this.router.navigate(['/']) // Usamos la función 'navigate' del servicio 'Router' para navegar a la ruta '/' al hacer clic en este elemento del menú.
+
+                routerLink: '/' // Usamos 'routerLink' para navegar a la ruta '/' al hacer clic en este elemento del menú.
               },
               {
                 label: 'Numeros',
-                icon: 'pi pi-dollar'
+                icon: 'pi pi-dollar',
+                // route: '/numbers' // Establecemos la ruta a la que se navegará al hacer clic en este elemento del menú.
+                // command: () => this.router.navigate(['/numbers']) // Usamos la función 'navigate' del servicio 'Router' para navegar a la ruta '/numbers'
+
+                routerLink: '/numbers' // Usamos 'routerLink' para navegar a la ruta '/numbers' al hacer clic en este elemento del menú.
               },
               {
                 label: 'No comunes',
-                icon: 'pi pi-globe'
+                icon: 'pi pi-globe',
+                // route: '/uncommon' // Establecemos la ruta a la que se navegará al hacer clic en este elemento del menú.
+                // command: () => this.router.navigate(['/uncommon']) // Usamos la función 'navigate' del servicio 'Router' para navegar a la ruta '/uncommon' al hacer clic en este elemento del menú.
+
+                routerLink: '/uncommon' // Usamos 'routerLink' para navegar a la ruta '/uncommon' al hacer clic en este elemento del menú.
               }
             ]
           },
