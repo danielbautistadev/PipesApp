691b2da357fc795c0bc86d44ea0ad8399304c94f
Erick Daniel <erick.olano@gmail.com>
2024-10-21 21:29:06 -0500

    cap. 164 - i18nPluralPipe - con un ejemplo detallado

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18nSelect
+
   public name: string = 'Daniel'; // Se declara una variable de tipo string y se le asigna un valor 
   public gender: 'male' | 'female' | 'sin especificar' = 'male'; // Se declara 3 tipos de datos que puede tener la variable, en este caso son 3 strings diferentes
   public invitationMap = {
@@ -25,5 +27,20 @@ export class UncommonPageComponent {
     'otros': 'Le enviastes un regalo.' 
   } // Se declara un objeto con propiedades de tipo string y se le asigna un valor de tipo string, en este caso se usa un objeto para mapear los valores de la variable recipientGender con los valores que se mostrarán en el HTML
 
+  // i18nPlural
+  public clients: string[] = ['Maria', 'Pedro', 'Fernando', 'Eduardo', 'Melissa', 'Natalia','Daniel','Martín','Juan', 'Pablo','Omar']; // Se declara un array de tipo string y se le asignan valores
+
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
+  } // Se declara un objeto con propiedades de tipo string y se le asigna un valor de tipo string, en este caso se usa un objeto para mapear los valores del array clients con los valores que se mostrarán en el HTML, el símbolo # se usa para indicar que se mostrará el valor de la variable clients en ese momento, el símbolo = se usa para indicar que se mostrará el valor que se le asigne a la propiedad, en este caso se usa para indicar que si el valor de la variable clients es igual a 0, 1 o 2 se mostrará el valor de la propiedad que tenga ese valor, y el símbolo other se usa para indicar que si el valor de la variable clients es diferente a 0, 1 o 2 se mostrará el valor de la propiedad other, en este caso se usa para indicar que si el valor de la variable clients es diferente a 0, 1 o 2 se mostrará el valor de la propiedad other, que en este caso es 'tenemos # clientes esperando.'
+
+  public deleteClient(): void  {
+    this.clients.shift(); // Se declara un método que elimina el primer elemento del array clients
+  }
+
+  
 
 }
